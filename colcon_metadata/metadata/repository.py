0d97993f64c6ccd870606bba7eda13902c413dbc
Maurice Alexander Purnawan <mauricepurnawan@gmail.com>
2026-09-13 11:06:26 +0800

    Handle missing `.netrc` when loading repositories (#41)
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- colcon_metadata/metadata/repository.py
+++ colcon_metadata/metadata/repository.py
@@ -84,6 +84,8 @@ def load_url(url, retry=2, retry_period=1, timeout=10, auth=NETRC):
         auth = None
         try:
             entry = netrc.netrc().authenticators(request.origin_req_host)
+        except FileNotFoundError:
+            logger.debug('No netrc file found, skipping...')
         except netrc.NetrcParseError:
             logger.exception('Failed to parse netrc file, skipping...')
         else:
